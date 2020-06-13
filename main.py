c78b755aa2a789b09302552e05cfc78f4f455d13
Igor Burenkov <isburenkov@gmail.com>
2020-06-13 15:47:49 +0000

    final version of main.py

--- main.py
+++ main.py
@@ -7,24 +7,13 @@ from data_forms import  vk_chatbot_unloading,   \
                         dictionary_of_lists,    \
                         marks_on_merlindiary
 
-
-class Test:
-    def __init__(self, obj):
-        self = obj # should obj be another reference to some instance to be a valid substitution? 
-
-
 file = open("input-horoshevo.json", "r")
-data = ""
-#file, data = [customize(obj) for obj in [file, data]]
-print(data)
-print(type(data))
-print(dir(data))
+file, data = [customize(obj) for obj in [file, ""]]
 
-# Модифицировать базовый мета-класс? Как ограничить scope?
-# Создать кастомный мета-класс на основе базового
-# Добавить оператор "правого присваивания"
-# объектам file и data назначить новый мета-класс
-#file >> data
+
+
+
+file >> data
 for form_of_data in (
     vk_chatbot_unloading,
     plaintext,
--- tweaks.py
+++ tweaks.py
@@ -1,31 +1,14 @@
-from copy import deepcopy
-from types import MethodType
 from fix import prefix, infix
 
 
 #Not sure how decorated decorated functions appear in the namespace
 #But I have a sense that with direct assignment in conventions.py I can use decorators freely
-def customize(obj):
-    # Not a bound or class method yet
-    def custom_init(self, instance):
-        self = instance
+class customize():
 
-    CustomizedClass = type(
-        "Customized" + obj.__class__.__name__,
-        (obj.__class__, ),
-        {
-            "__init__": custom_init,
-            "__rshift__": right_assignment
-        }
-    )
-
-    obj = CustomizedClass(obj)
-
-    return obj
-
-
-def right_assignment(a, b):
-    b.__init__(a)
+    def __init__(self, instance):
+        self.innerObject = instance
+    def __rshift__(a, b):
+        b.innerObject = a.innerObject
 
 
 def to_func(a, b):
@@ -35,6 +18,7 @@ to = infix(to_func)
 
 def transform_func(x):
     (data, function) = x
-    return function(data)
+    data.innerObject = function(data.innerObject)
+    return data
 transform = prefix(transform_func)
 
